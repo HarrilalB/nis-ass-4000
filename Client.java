c3ad4d5f097f208e008737002f095fd184389e84
hrrbha001 <harrilalbhavana@gmail.com>
2013-03-06 13:59:36 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,207 +1,251 @@
 /*
-  Server Class: Simulates a Server that collects information from various sensors(Clients) and uploads a Report regularly
+	Client Class: Simulates a Client that sends information to Server and downloads a Report regularly
 	Written By: Eric Su(SXXERI002), Lynray Barends (BRNLYN013), Phaswana Malatjie(MLTPHA002)
 	Date: 5 April 2012
 */
-
 import java.io.*;
 import java.net.*;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.lang.Math;
 import java.util.Scanner;
+//libraries used for creating a graph gui
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import javax.swing.*;
 
-public class Server
+
+public class Client extends JPanel implements Runnable
 {
-
     static Socket client_socket = null;
-    static ServerSocket server_socket = null;
+    static PrintStream output_stream = null;
+    static Scanner input_stream = null;
+    static BufferedReader input_line = null;
+    static boolean closed = false;
+    static double[] data = new double [20];
+    final int PAD = 20;
 
-    static clientThread t[] = new clientThread [3]; //accepting about 3 clients to connect
+    static final String USER = "BRNLYN013"; //could be anyone's Student Number
 
     public static void main (String[] args)
     {
-
-	int port_num = 32505; //this could be anyone's port number.
-
-	if (args.length < 1)
+	int port_num = 32505; //Students' Port Number
+	String host = "nightmare.cs.uct.ac.za";
+	String sensor;
+	if (args.length < 3)
 	{
-	    System.out.println ("server is running on port: " + port_num);
+	    sensor = args [0]; //Sensor corresponding to 1st Argument
+	    System.out.println ("got here: " + host + " " + port_num);
 	}
 	else
 	{
-	    port_num = Integer.valueOf (args [0]).intValue ();
+	    sensor = args [0]; //Sensor corresponding to 1st Argument
+	    host = args [1];
+	    port_num = Integer.valueOf (args [2]).intValue ();
 	}
-
 	try
 	{
-	    server_socket = new ServerSocket (port_num);
+	    client_socket = new Socket (host, port_num); //Creates New Socket
+	    //Creates Streams
+	    input_line = new BufferedReader (new InputStreamReader (System.in));
+	    output_stream = new PrintStream (client_socket.getOutputStream ());
+	    input_stream = new Scanner (client_socket.getInputStream ());
+	}
+	catch (UnknownHostException e)
+	{ //Unknown Host
+	    System.err.println ("don't know about host" + host);
 	}
 	catch (IOException e)
-	{
-	    System.out.println (e);
+	{ //Unable to get a Connection
+	    System.err.println ("couldn't get I/O for the connection to the host" + host);
 	}
 
-	Multi:
-	while (true)
+	//If everything is in Place
+	if (client_socket != null && output_stream != null && input_stream != null)
 	{
 	    try
-	    {
-		client_socket = server_socket.accept ();
-		//Creates Dedicated thread for each Sensor
-		for (int i = 0 ; i <= 2 ; i++)
-		{
-		    if (t [i] == null)
-		    {
-			if (i == 0)
-			    (t [i] = new clientThread ("water", client_socket, t)).start ();
-			else if (i == 1)
-			    (t [i] = new clientThread ("sound", client_socket, t)).start ();
-			else if (i == 2)
-			{
-			    (t [i] = new clientThread ("bend", client_socket, t)).start ();
-			    break Multi;
-			}
-			break;
-		    }
-		}
-	    } //this allows multiple clients running
-	    catch (IOException e)
-	    {
-		System.out.println (e);
-	    }
-	    //Thread that will upload HTML report and notify Client when it is ready to be uploaded
-	    Thread fileLoader = new Thread (new Runnable ()
-	    {
+	    { //Start New CLient Thread
+		new Thread (new Client ()).start ();
+		DateFormat dateFormat = new SimpleDateFormat ("HH:mm:ss");
+		Calendar cal;
+		String time;
+		int i = 0;
 
-		public void run ()
-		{
-
-		    //Sets up log file
-		    PrintStream log = null;
+		while (!closed)
+		{ //Sends Recording Every Second while connection with Server is not Closed
+		    cal = Calendar.getInstance ();
+		    time = dateFormat.format (cal.getTime ());
+		    output_stream.println (time + "," + Client.generateValue (sensor));
 		    try
 		    {
-			log = new PrintStream (new FileOutputStream ("sensors/log.txt"));
+			Thread.sleep (1000);
+			data [i] = Client.generateValue (sensor);
+			i++;
 		    }
 		    catch (Exception e)
 		    {
 			e.printStackTrace ();
 		    }
-
-		    //Sets up Date Format to record Current Time
-		    DateFormat dateFormat = new SimpleDateFormat ("HH:mm:ss");
-		    Calendar cal;
-		    String time;
-		    while (true)
-		    {
-			//Will Upload newest report every 20 Seconds
-			try
-			{
-			    Thread.sleep (20000);
-			}
-			catch (Exception e)
-			{
-			    e.printStackTrace ();
-			}
-
-			Html.generate (); //Generates HTML
-
-			//Records Time To Log File
-			cal = Calendar.getInstance ();
-			time = dateFormat.format (cal.getTime ());
-			log.append (time + ": Server Uploaded Report to http://people.cs.uct.ac.za/~" + Html.STUDENT_NUMBER + ".Report.html\n");
-			//instructs client to download report
-			if (t [0] != null)
-			    t [0].output_stream.println ("update report");
-			cal = Calendar.getInstance ();
-			time = dateFormat.format (cal.getTime ());
-			log.append (time + ": Client Downloaded Report\n"); //Logs the Time
-		    }
-		}
-	    }
-	    );
-
-	    fileLoader.start (); //Starts The Thread
-
-	}
-    }
-
-
-    //Each Client Thread Responds to a Sensor (Client Class)
-    static class clientThread extends Thread
-    {
-
-	Scanner input_stream = null;
-	PrintStream output_stream = null;
-	Socket client_socket = null;
-
-	clientThread t[];
-	String name; //Name of the Sensor it is Responsible for
-	//Constructor
-	public clientThread (String n, Socket client_socket, clientThread[] t)
-	{
-	    this.client_socket = client_socket;
-	    this.t = t;
-	    name = n;
-	}
-
-	public void run ()
-	{
-	    String line = "";
-	    String dir;
-	    PrintStream file = null;
-	    //Sets Up File Directory Name According to the Sensor it responds to
-	    if (name.equals ("water"))
-		dir = "water.data";
-	    else if (name.equals ("sound"))
-		dir = "sound.data";
-	    else
-		dir = "bend.data";
-
-	    DateFormat dateFormat = new SimpleDateFormat ("HH:mm:ss");
-	    Calendar cal;
-	    String time;
-	    int count = 0; //Keeps track of the Values Recorded
-	    try
-	    {
-		//Opens The Streams
-		input_stream = new Scanner (client_socket.getInputStream ());
-		output_stream = new PrintStream (client_socket.getOutputStream ());
-		file = new PrintStream (new FileOutputStream ("sensors/" + dir));
-
-
-		while (count != 20)
-		{ //Can Change depending on how any values you would like each Client to produce
-		    line = input_stream.nextLine (); //input received from client
-		    //Gets Current Time
-		    cal = Calendar.getInstance ();
-		    time = dateFormat.format (cal.getTime ());
-
-		    //Saves input from client ( as well as time stamp that it was received at) to respective sensor file
-		    file.append (line.substring (0, line.indexOf (",")) + "," + time + "," + line.substring (line.indexOf (",") + 1) + "\n");
-		    output_stream.println ("Processed"); //Can Edit out - just used for debugging purposes.
-
-		    count++; //increases count
-
 		}
 
-		//Closes Current thread
-		for (int i = 0 ; i <= 2 ; i++)
-		    if (t [i] == this)
-			t [i] = null;
-		//Closes Files
-		file.close ();
-		//Sends Message to client that it has processed the requested amount of values
-		output_stream.println ("Finished");
-		//Closes the Streams
-		input_stream.close ();
 		output_stream.close ();
+		input_stream.close ();
 		client_socket.close ();
 	    }
 	    catch (IOException e)
 	    {
-		e.printStackTrace ();
+		System.err.println ("IOException: " + e);
 	    }
 	}
+
+	JFrame graphWindow = new JFrame ();
+	graphWindow.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+	graphWindow.add (new Client ());
+	graphWindow.setSize (400, 400);
+	graphWindow.setLocation (200, 200);
+	graphWindow.setVisible (true);
+    }
+
+
+    //Drawing graph contents
+    public void paintComponent (Graphics g)
+    {
+	super.paintComponent (g);
+	Graphics2D g2 = (Graphics2D) g;
+	g2.setRenderingHint (RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	int w = getWidth ();
+	int h = getHeight ();
+	// Draw ordinate.
+	g2.draw (new Line2D.Double (PAD, PAD, PAD, h - PAD));
+	// Draw abcissa.
+	g2.draw (new Line2D.Double (PAD, h - PAD, w - PAD, h - PAD));
+	// Draw labels.
+	Font font = g2.getFont ();
+	FontRenderContext frc = g2.getFontRenderContext ();
+	LineMetrics lm = font.getLineMetrics ("0", frc);
+	float sh = lm.getAscent () + lm.getDescent ();
+	// Ordinate label.
+	String s = "data";
+	float sy = PAD + ((h - 2 * PAD) - s.length () * sh) / 2 + lm.getAscent ();
+	for (int i = 0 ; i < s.length () ; i++)
+	{
+	    String letter = String.valueOf (s.charAt (i));
+	    float sw = (float) font.getStringBounds (letter, frc).getWidth ();
+	    float sx = (PAD - sw) / 2;
+	    g2.drawString (letter, sx, sy);
+	    sy += sh;
+	}
+	// Abcissa label.
+	s = "time";
+	sy = h - PAD + (PAD - sh) / 2 + lm.getAscent ();
+	float sw = (float) font.getStringBounds (s, frc).getWidth ();
+	float sx = (w - sw) / 2;
+	g2.drawString (s, sx, sy);
+	// Draw lines.
+	double xInc = (double) (w - 2 * PAD) / (data.length - 1);
+	double scale = (double) (h - 2 * PAD) / getMax ();
+	g2.setPaint (Color.green.darker ());
+	for (int i = 0 ; i < data.length - 1 ; i++)
+	{
+	    double x1 = PAD + i * xInc;
+	    double y1 = h - PAD - scale * data [i];
+	    double x2 = PAD + (i + 1) * xInc;
+	    double y2 = h - PAD - scale * data [i + 1];
+	    g2.draw (new Line2D.Double (x1, y1, x2, y2));
+	}
+	// Mark data points.
+	g2.setPaint (Color.red);
+	for (int i = 0 ; i < data.length ; i++)
+	{
+	    double x = PAD + i * xInc;
+	    double y = h - PAD - scale * data [i];
+	    g2.fill (new Ellipse2D.Double (x - 2, y - 2, 4, 4));
+	}
+    }
+
+
+    private static int getMax ()
+    {
+	int max = -Integer.MAX_VALUE;
+	for (int i = 0 ; i < data.length ; i++)
+	{
+	    if (data [i] > max)
+		max = (int) data [i];
+	}
+	return max;
+    }
+
+
+    //Generates The Recording depending on Sensor Type
+    public static double generateValue (String type)
+    {
+	int min, max;
+	if (type.equalsIgnoreCase ("water"))
+	{
+	    //Generates Water % between 0 and 100
+	    min = 0;
+	    max = 100;
+	    return Math.round ((min + Math.random () * (max - min)) * 100) / 100.00;
+	}
+	else if (type.equalsIgnoreCase ("sound"))
+	{
+	    //Generates Sound Frequency between 20 and 20000HZ
+	    min = 20;
+	    max = 20000;
+	    return Math.round ((min + Math.random () * (max - min)) * 100) / 100.00;
+	}
+	else if (type.equalsIgnoreCase ("bend"))
+	{
+	    //Generates =Refractive Index corresponding to the bend between 1 and 2
+	    min = 1;
+	    max = 2;
+	    return Math.round ((min + Math.random () * (max - min)) * 100) / 100.00;
+	}
+	return 0;
+    }
+
+
+    //Downloads The Report From the Internet
+    public static void downloadReport () throws IOException
+    {
+	URL report = new URL ("http://people.cs.uct.ac.za/~" + USER + "/Report.html");
+	ReadableByteChannel rbc = Channels.newChannel (report.openStream ());
+	FileOutputStream fos = new FileOutputStream ("report.html");
+	fos.getChannel ().transferFrom (rbc, 0, 1 << 24);
+	System.out.println ("Report Updated");
+    }
+
+
+    public void run ()
+    {
+	String responseLine;
+
+	//Gets Input From Server
+	while ((responseLine = input_stream.nextLine ()) != null)
+	{
+	    if (responseLine.equalsIgnoreCase ("update report"))
+		//Downloads the Report to the Same Directory that CLient is saved in
+		try
+		{
+		    downloadReport ();
+		}
+	    catch (IOException e)
+	    {
+		e.printStackTrace ();
+	    }
+
+	    else
+	    {
+		System.out.println (responseLine); //print out report
+		if (responseLine.indexOf ("Finished") != -1)
+		    break;                                            //if all is finished closes Client
+	    }
+	}
+	closed = true;
+
     }
 }
